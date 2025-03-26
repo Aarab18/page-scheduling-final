5279ac9be9a6061642375b30333d9af67916ded3
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-03-26 22:15:15 +0530

    Implemented algorithm execution logic

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ if __name__ == "__main__":
 
 from algorithms import fifo, lru, optimal, second_chance, clock
 from gui import MainWindow
+def run_algorithms(ref_string, frame_size):
+    results = {
+        "FIFO": fifo(ref_string, frame_size),
+        "LRU": lru(ref_string, frame_size),
+        "Optimal": optimal(ref_string, frame_size),
+        "Second Chance": second_chance(ref_string, frame_size),
+        "Clock": clock(ref_string, frame_size)
+    }
+    return results
