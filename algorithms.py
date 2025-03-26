178cf48136f035e8f593bf2e165d7e1862b6198a
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-03-26 19:54:02 +0530

    Initialized project seup with algorithms.py

---
+++ algorithms.py
@@ -0,0 +1,14 @@
+def fifo(reference_string, frame_size):
+    pass
+
+def lru(reference_string, frame_size):
+    pass
+
+def optimal(reference_string, frame_size):
+    pass
+
+def second_chance(reference_string, frame_size):
+    pass
+
+def clock(reference_string, frame_size):
+    pass
