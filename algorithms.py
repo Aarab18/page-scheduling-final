fc8097c818041ebb61c405d0ca4dc49d1d9d576a
Aarab Ahmad <ahmadaarab315@gmail.com>
2025-03-26 19:55:28 +0530

    Implemented LRU Algorithm

--- algorithms.py
+++ algorithms.py
@@ -12,7 +12,20 @@ def fifo(reference_string, frame_size):
     return page_faults
 
 def lru(reference_string, frame_size):
-    pass
+    frames = []
+    page_faults = 0
+    for page in reference_string:
+        if page not in frames:
+            if len(frames) < frame_size:
+                frames.append(page)
+            else:
+                frames.pop(0)
+                frames.append(page)
+            page_faults += 1
+        else:
+            frames.remove(page)
+            frames.append(page)
+    return page_faults
 
 def optimal(reference_string, frame_size):
     pass
